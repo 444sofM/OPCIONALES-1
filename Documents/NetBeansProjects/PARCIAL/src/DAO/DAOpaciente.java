9f11ccefd846f98c6b0dcd22fe122601b71fd6ee
444sofM <ana.mezu@correounivalle.edu.co>
2023-06-23 00:24:06 -0500

    final2

--- Documents/NetBeansProjects/PARCIAL/src/DAO/DAOpaciente.java
+++ Documents/NetBeansProjects/PARCIAL/src/DAO/DAOpaciente.java
@@ -1,16 +1,13 @@
 /*
+/*
+//Ana Mezu
+//PARCIAL
+
+//LUIS YOVANNY ROMO
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  *
