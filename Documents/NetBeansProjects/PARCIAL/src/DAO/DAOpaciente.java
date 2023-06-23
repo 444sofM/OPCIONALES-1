4f6353f701bd8a8bd53147119a20c4b68490ddcd
444sofM <ana.mezu@correounivalle.edu.co>
2023-06-23 00:10:00 -0500

    final1

--- Documents/NetBeansProjects/PARCIAL/src/DAO/DAOpaciente.java
+++ Documents/NetBeansProjects/PARCIAL/src/DAO/DAOpaciente.java
@@ -4,10 +4,19 @@
  */
 package DAO;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Usuario
  */
 public class DAOpaciente {
+
     
 }
